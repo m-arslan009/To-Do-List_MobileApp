e13f78a30ca708d03e59ba8a4ae0d39bea525cbc
Muhammad Arslan <iqbalarslan009@gmail.com>
2024-02-07 11:52:48 +0500

    user interface is ready

---
+++ index.js
@@ -0,0 +1,7 @@
+const inputField = document.getElementById("input-field");
+const addBtn = document.getElementById("add-btn");
+
+addBtn.addEventListener("click", function() {
+    let inputValue = inputField.value;
+    console.log(inputValue);
+})
