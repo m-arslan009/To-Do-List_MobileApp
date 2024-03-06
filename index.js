0e17ecebd123f79ff67f65f8a929a13ee2ab1652
Muhammad Arslan <iqbalarslan009@gmail.com>
2024-03-06 19:05:26 +0500

    able to get value from input field and store to firebase

--- index.js
+++ index.js
@@ -1,57 +1,23 @@
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-// import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js"
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyB-Mnq1-hRI9ugWNXOCpNDUIa4km33yeas",
-//   authDomain: "todo-mobileapp-1021b.firebaseapp.com",
-//   databaseURL: "https://todo-mobileapp-1021b-default-rtdb.europe-west1.firebasedatabase.app",
-//   projectId: "todo-mobileapp-1021b",
-//   storageBucket: "todo-mobileapp-1021b.appspot.com",
-//   messagingSenderId: "700108880574",
-//   appId: "1:700108880574:web:fbb080e85d7376d83ffcef"
-// };
-
-// const app = initializeApp(firebaseConfig)
-// const dataBase = getDatabase(app);
-// const moviesInDB = ref(dataBase, "Movies")
-// console.log(app);
-
-import {initializeApp} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
-
-const appSetting = {
-    apiKey: "AIzaSyB-Mnq1-hRI9ugWNXOCpNDUIa4km33yeas",
-    authDomain: "todo-mobileapp-1021b.firebaseapp.com",
-    databaseURL: "https://todo-mobileapp-1021b-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "todo-mobileapp-1021b",
-    storageBucket: "todo-mobileapp-1021b.appspot.com",
-    messagingSenderId: "700108880574",
-    appId: "1:700108880574:web:fbb080e85d7376d83ffcef"
-};
-
-const app = initializeApp(appSetting);
-const dataBase = getDatabase(app);
-const shoppinglistDB = ref(dataBase, "ShoppingList");
-
-const inputField = document.getElementById("input-field");
-const addBtn = document.getElementById("add-btn");
-
-// addBtn.addEventListener("click", function(event) {
-//     console.log(event)
-//     // let inputValue = inputField.value;
-//     // push(moviesInDB, inputValue)
-//     // console.log(`${inputValue} added to database`);
-// })
-
-addBtn.addEventListener("click", function() {
-    // let inputValue = inputField.value;
-    // push(moviesInDB, inputValue)
-    // console.log(`${inputValue} added to database`);
-    let inputValue = inputField.value
-    push(shoppinglistDB, inputValue)
-    console.log(`${inputValue} added to database`);
-})
-
-const config = {
-    url: "https://assignment-course-6359d-default-rtdb.asia-southeast1.firebasedatabase.app/"
+const appSettings = {
+    databaseURL: "https://todo-mobileapp-1021b-default-rtdb.europe-west1.firebasedatabase.app/"
+    
 }
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-btn")
+const shoppingListEl = document.getElementById("shopping-list")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+    inputFieldEl.value = "";
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
+})
