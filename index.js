08747178eb0ece34effd2c701bc2408008dad0ba
Muhammad Arslan <iqbalarslan009@gmail.com>
2024-03-06 17:50:51 +0500

    conecting to firebase

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+// import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js"
 
-const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DB_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MSG_SENDER_ID,
-    appId: process.env.APP_ID
+// const firebaseConfig = {
+//     apiKey: "AIzaSyB-Mnq1-hRI9ugWNXOCpNDUIa4km33yeas",
+//   authDomain: "todo-mobileapp-1021b.firebaseapp.com",
+//   databaseURL: "https://todo-mobileapp-1021b-default-rtdb.europe-west1.firebasedatabase.app",
+//   projectId: "todo-mobileapp-1021b",
+//   storageBucket: "todo-mobileapp-1021b.appspot.com",
+//   messagingSenderId: "700108880574",
+//   appId: "1:700108880574:web:fbb080e85d7376d83ffcef"
+// };
+
+// const app = initializeApp(firebaseConfig)
+// const dataBase = getDatabase(app);
+// const moviesInDB = ref(dataBase, "Movies")
+// console.log(app);
+
+import {initializeApp} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+
+const appSetting = {
+    apiKey: "AIzaSyB-Mnq1-hRI9ugWNXOCpNDUIa4km33yeas",
+    authDomain: "todo-mobileapp-1021b.firebaseapp.com",
+    databaseURL: "https://todo-mobileapp-1021b-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "todo-mobileapp-1021b",
+    storageBucket: "todo-mobileapp-1021b.appspot.com",
+    messagingSenderId: "700108880574",
+    appId: "1:700108880574:web:fbb080e85d7376d83ffcef"
 };
 
-const app = initializeApp(firebaseConfig)
-console.log(app);
+const app = initializeApp(appSetting);
+const dataBase = getDatabase(app);
+const shoppinglistDB = ref(dataBase, "ShoppingList");
 
 const inputField = document.getElementById("input-field");
 const addBtn = document.getElementById("add-btn");
 
+// addBtn.addEventListener("click", function(event) {
+//     console.log(event)
+//     // let inputValue = inputField.value;
+//     // push(moviesInDB, inputValue)
+//     // console.log(`${inputValue} added to database`);
+// })
+
 addBtn.addEventListener("click", function() {
-    let inputValue = inputField.value;
-    console.log(inputValue);
+    // let inputValue = inputField.value;
+    // push(moviesInDB, inputValue)
+    // console.log(`${inputValue} added to database`);
+    let inputValue = inputField.value
+    push(shoppinglistDB, inputValue)
+    console.log(`${inputValue} added to database`);
 })
+
+const config = {
+    url: "https://assignment-course-6359d-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
